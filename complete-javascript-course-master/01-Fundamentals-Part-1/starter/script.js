20afe1845e074a67ca60e78c602d79fbbd8c6c24
SaurabhTiwari123-juet <56027820+SaurabhTiwari123-juet@users.noreply.github.com>
2022-10-10 14:34:19 +0530

    new4

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,14 @@
+const tt = true;
+console.log(tt);
+
+let kk = 23;
+console.log(kk);
+
+const now = 2037;
+const myage = now - 1998;
+const amanage = now - 1994;
+console.log(myage, amanage);
+
+// 2**3 means 2 to the power 3 which is 8
+console.log(2 ** 3);
+// event handling in js
