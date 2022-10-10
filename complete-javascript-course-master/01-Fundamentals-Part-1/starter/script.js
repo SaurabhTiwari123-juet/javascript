13f6690adc55f3b9379ecc2540f995e91d97a441
SaurabhTiwari123-juet <56027820+SaurabhTiwari123-juet@users.noreply.github.com>
2022-10-10 17:46:10 +0530

    new3

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -11,4 +11,10 @@ console.log(myage, amanage);
 
 // 2**3 means 2 to the power 3 which is 8
 console.log(2 ** 3);
-// event handling in js
+// strings and templete literals they are easy to use
+
+const name = "Saurabh";
+const age = 22;
+const occupation = "student";
+const saurabhdefine = `I am a ${occupation} my name is ${name} and my age is ${age}`;
+console.log(saurabhdefine);
