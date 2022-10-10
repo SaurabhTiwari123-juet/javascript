0ca8ee0a55d093755906224b50237ad2223bcd7a
SaurabhTiwari123-juet <56027820+SaurabhTiwari123-juet@users.noreply.github.com>
2022-10-10 22:01:47 +0530

    new4

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -18,3 +18,43 @@ const age = 22;
 const occupation = "student";
 const saurabhdefine = `I am a ${occupation} my name is ${name} and my age is ${age}`;
 console.log(saurabhdefine);
+
+// multiple line strings
+console.log("string with \n\
+multiple \n\
+lines");
+// another way is using template literals
+console.log(`string 
+with  multiple 
+lines part 2`);
+// taking decisions if else statements
+
+const ageefordrive = 2;
+const isoldenough = ageefordrive >= 18;
+if (isoldenough) console.log(`congrats you are able to drive car 👍`);
+else {
+  console.log(`oops 😒 you are not eligible for driving`);
+}
+const birthYears = 1999;
+let century;
+if (birthYears <= 2000) century = 20;
+else {
+  century = 21;
+}
+console.log(century);
+
+// bmi question 2
+
+const massmark = 95;
+const heightmark = 1.88;
+const massjohn = 85;
+const heightJohn = 1.75;
+const bmimark = massmark / heightmark ** 2;
+const BMIJohn = massjohn / heightJohn ** 2;
+const markHigherBMI = bmimark > BMIJohn;
+
+if (markHigherBMI)
+  console.log(`marks  bmi ${bmimark} is higher then john bmi ${BMIJohn}`);
+else {
+  console.log(`marks  bmi ${bmimark} is lower then john bmi ${BMIJohn}`);
+}
