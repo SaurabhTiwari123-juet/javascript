fff3f5d7146ba0075b13602aac9f8f3ea06039d5
SaurabhTiwari123-juet <56027820+SaurabhTiwari123-juet@users.noreply.github.com>
2022-11-16 11:35:22 +0530

    guess my number game

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,13 +1,35 @@
 'use strict';
 // selecting and manipatuting items
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number!😊';
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!😊';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
 
 // handeling click events
+// event liseners
+const secretnumber = Math.Trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretnumber;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number';
+  } else if (guess === secretnumber) {
+    document.querySelector('.message').textContent = 'Correct Number!😊';
+  } else if (guess > secretnumber) {
+    document.querySelector('.message').textContent = '📈 too High';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretnumber) {
+    document.querySelector('.message').textContent = '📉 too Low';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
